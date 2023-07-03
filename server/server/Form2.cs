36fb4b2562283aeab5b7096a35c69bc8113c6999
Eşref <69198813+esrefaltug@users.noreply.github.com>
2023-07-03 14:25:42 +0300

    Add files via upload

---
+++ server/server/Form2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace server
+{
+
+    public partial class Form2 : Form
+    {
+        private readonly int port;
+        private TcpClient client;//Tcp servislerine bağlantı sağlamak için TcPClient sınıfı kullanılır.
+
+        private TcpListener server;
+        private NetworkStream mainStream;
+       
+
+        private readonly Thread Listening;
+        private readonly Thread GetImage;
+      
+
+        public Form2(int Port)
+        {
+            port = Port;
+            client = new TcpClient();
+          
+            Listening = new Thread(StartListening);
+            GetImage = new Thread(ReceiveImage);
+            
+            InitializeComponent();
+        }
+
+       
+
+        private void StartListening()
+        {
+            
+            while (!client.Connected)
+            {
+                server.Start();
+                client = server.AcceptTcpClient();//sunucu istemciyle iletişim kurabilir ve veri alışverişi yapabilir.
+            }
+            GetImage.Start(); 
+          
+        }
+
+        private void StopListening()
+        {
+            server.Stop();
+            client = null;
+         
+            if (Listening.IsAlive) Listening.Abort();//thread sonlandırılır
+            if (GetImage.IsAlive) Listening.Abort();
+        }
+
+        private void ReceiveImage()
+        {
+            BinaryFormatter binFormatter = new BinaryFormatter();//BinaryFormatter nesnesi oluşturur ve binFormatter adlı değişkene atar. Bu nesne, ikili veri işlemlerini gerçekleştirmek için kullanılacaktır.
+            while (client.Connected )
+            {
+                mainStream = client.GetStream();
+                pictureBox1.Image =(Image) binFormatter.Deserialize(mainStream);
+
+               
+            }
+                
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            server = new TcpListener(IPAddress.Any,port); //form uygulanmasının yüklenmesi
+            Listening.Start();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopListening();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
